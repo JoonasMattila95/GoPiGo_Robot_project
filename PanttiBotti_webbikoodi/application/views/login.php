53a1dea7d4d0b5d8f54a65c093e47f2cab9fd735
Joonas Mattila <jonttu.mat@hotmail.com>
2018-11-21 10:38:44 +0200

    välidumppi tänhetkiselle koodille

---
+++ PanttiBotti_webbikoodi/application/views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/bootstap.css">
+    <link rel="stylesheet" type="text/css" href="css/tyyli.css">
+    <title>PanttiBotti_Login</title>
+  </head>
+  <body>
+    <div class="container">
+      <div id="login_row">
+
+      <form class="" action="login_data.php" method="post">
+        <label for="username_label">Username:</label><br>
+        <input type="text" name="username_input" placeholder="Username"><br>
+        <label for="password_label">Password:</label><br>
+        <input type="text" name="password_input" placeholder="Password"><br>
+      </div>
+      </form>
+    </div>
+  </body>
+</html>
