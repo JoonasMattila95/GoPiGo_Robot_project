097e09d21be85b70880083a3aad4669adf4248d5
Joonas Mattila <jonttu.mat@hotmail.com>
2018-12-11 11:26:38 +0200

    Webbikoodi fiksattuna raspille

---
+++ Panttibotin_webbikoodi/application/views/topbar.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/bootstrap.css">
+<div class = ".container">
+  <div class = "row">
+     <div class="col-xl">
+       <div style=" background-color: blue;">
+       <h1> Valikko1 </h1>
+     </div>
+        </div>
+     <div class="col-xl">
+       <h1> valikko2 </h1>
+     </div>
+     <div class="col-sm">
+        <h1> valikko3 </h1>
+     </div>
+  </div>
